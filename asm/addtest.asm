c8d346319d419cd7ac79bf8f2d9c0c6b7a21dce4
Aleksa Marković <28643848+topofkeks@users.noreply.github.com>
2021-08-21 13:28:19 +0200

    ALU bugfix, new control unit, debug setup

--- asm/addtest.asm
+++ asm/addtest.asm
@@ -1,6 +1,6 @@
-li a0, 100	#  64
-li a1, -100	#  ff9c
-add a0, x0, a0	# 64
-add a1, x0, a1	# ff9c
-add a2, a0, a1	# 0
+li a0, 0x100	#  100
+li a1, -0x100	#  ff00
+add a0, x0, a0	#  100
+add a1, x0, a1	#  ff00
+add a2, a0, a1	#  0
 fence 1,1
